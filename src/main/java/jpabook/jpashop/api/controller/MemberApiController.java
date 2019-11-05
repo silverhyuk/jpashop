1d9a3feb5ccf98d6900f0c558a88142974143e46
ehchoi <ehchoi@cafe24corp.com>
2019-11-05 12:39:46 +0900

    회원 조회 API

--- src/main/java/jpabook/jpashop/api/controller/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/controller/MemberApiController.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop.api.controller;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
 import lombok.AllArgsConstructor;
@@ -9,12 +11,30 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> getMemberV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result getMemberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName(), m.getAddress()))
+                .collect(Collectors.toList());
+
+        return new Result(collect.size(), collect);
+    }
+
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
@@ -49,7 +69,6 @@ public class MemberApiController {
     }
 
     @Data
-    @AllArgsConstructor
     static class CreateMemberRequest {
         @NotEmpty
         private String name;
@@ -63,8 +82,21 @@ public class MemberApiController {
     }
 
     @Data
-    @AllArgsConstructor
     static class UpdateMemberRequest {
         private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+        private Address address;
     }
 }
