2eb4a200b0dbdba2e73af472ab2593a5796ee938
silverhyuk84 <silverhyuk84@gmail.com>
2019-11-05 20:55:34 +0900

    샘플 데이터 입력

--- src/main/java/jpabook/jpashop/InitDb.java
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -18,6 +18,7 @@ public class InitDb {
     @PostConstruct
     public void init() {
         initService.dbInit1();
+        initService.dbInit2();
     }
 
 
@@ -29,30 +30,59 @@ public class InitDb {
         private final EntityManager em;
 
         public void dbInit1() {
-            Member member = new Member();
-            member.setName("userA");
-            member.setAddress(new Address("서울", "1", "1111"));
-            em.persist(member);
+            Member member = getMember("userA", "서울", "1", "1111");
 
-            Book book1 = new Book();
-            book1.setName("JPA1 BOOK");
-            book1.setPrice(10000);
-            book1.setStockQuantity(100);
-            em.persist(book1);
+            Book book1 = getBook("JPA1 BOOK", 10000, 100);
 
-            Book book2 = new Book();
-            book2.setName("JPA2 BOOK");
-            book2.setPrice(20000);
-            book2.setStockQuantity(50);
-            em.persist(book2);
+            Book book2 = getBook("JPA2 BOOK", 20000, 100);
 
             OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
             OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
 
 
+            Delivery delivery = getDelivery(member);
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        public void dbInit2() {
+            Member member = getMember("userB", "진주", "2", "2222");
+
+            Book book1 = getBook("SPRING1 BOOK", 30000, 100);
+
+            Book book2 = getBook("SPRING2 BOOK", 40000, 100);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+
+            Delivery delivery = getDelivery(member);
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        private Book getBook(String s, int i, int stockQuantity) {
+            Book book1 = new Book();
+            book1.setName(s);
+            book1.setPrice(i);
+            book1.setStockQuantity(stockQuantity);
+            em.persist(book1);
+            return book1;
+        }
+
+        private Member getMember(String name, String city, String street, String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            em.persist(member);
+            return member;
+        }
+
+        private Delivery getDelivery(Member member) {
             Delivery delivery = new Delivery();
             delivery.setAddress(member.getAddress());
-            Order.createOrder(member, delivery, orderItem1, orderItem2);
+            return delivery;
         }
+
     }
 }
