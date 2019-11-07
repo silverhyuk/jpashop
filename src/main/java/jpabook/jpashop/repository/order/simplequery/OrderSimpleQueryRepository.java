06844e646f75635a2ff0261bf7324f4c62984c33
ehchoi <ehchoi@cafe24corp.com>
2019-11-07 12:19:00 +0900

    v4 코드정리

---
+++ src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository.order.simplequery;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderSimpleQueryRepository {
+
+
+
+    private final EntityManager entityManager;
+    /**
+     * 일반적인 SQL을 사용할 때 처럼 원하는 값을 선택해서 조회
+     * new 명령어를 사용해서 JPQL의 결과를 DTO로 즉시 변환
+     * SELECT 절에서 원하는 데이터를 직접 선택하므로 DB 애플리케이션 네트웍 용량 최적화(생각보다 미비)
+     * 리포지토리 재사용성 떨어짐, API 스펙에 맞춘 코드가 리포지토리에 들어가는 단점
+     * @return
+     */
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return entityManager.createQuery(
+                "select new jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                        " from Order o" +
+                        " join o.member m" +
+                        " join o.delivery d", OrderSimpleQueryDto.class
+        ).getResultList();
+    }
+
+}
